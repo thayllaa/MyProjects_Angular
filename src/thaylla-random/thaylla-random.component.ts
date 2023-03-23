17d2951c614896fff1a9e37f3bcb124372e6128a
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-22 21:28:23 -0300

    1 file modified, 1 added

--- src/thaylla-random/thaylla-random.component.ts
+++ src/thaylla-random/thaylla-random.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thaylla-random.component.css']
 })
 export class ThayllaRandomComponent implements OnInit {
+  n: number = 10;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getNumbers() {
+    let arr = [];
+    for (let i = 1; i <= this.n; i++) {
+      arr.push(Math.trunc(100 * Math.random()));
+    }
+    return arr;
+  }
 }
